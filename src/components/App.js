ec2c2f68d65f27f6b44e48a862c6f5d9959515cf
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:36:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [fields, setFields] = useState([]);
+
+  const handleAddField = () => {
+    setFields([...fields, { id: fields.length + 1, value: "" }]);
+  };
+
+  const handleDeleteField = (index) => {
+    console.log("You are deleting " + fields[index].value + " field");
+    setFields(fields.filter((field, i) => i !== index));
+  };
+
+  const handleEditField = (index, value) => {
+    const newFields = [...fields];
+    newFields[index].value = value;
+    setFields(newFields);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {fields.length === 0 && <p>No fields in the form</p>}
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <input
+            type="text"
+            value={field.value}
+            onChange={(e) => handleEditField(index, e.target.value)}
+          />
+          <button onClick={() => handleDeleteField(index)} className='delete'>Delete</button>
+        </div>
+      ))}
+      <button onClick={handleAddField} className='add'>Add Field</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
